4338072672c3541af26d9a26432b36a30fe05452
elgboffel <johnny2900@gmail.com>
2018-01-16 19:10:52 +0100

    Dev commit

--- Umbraco/TNNPlay.Web/config/grid.editors.config.js
+++ Umbraco/TNNPlay.Web/config/grid.editors.config.js
@@ -452,5 +452,65 @@
             ],
             "frontView": ""
         }
+    },
+    {
+        "name": "Quote With Icon",
+        "alias": "quoteWithIcon",
+        "view": "/App_Plugins/LeBlender/editors/leblendereditor/LeBlendereditor.html",
+        "icon": "icon-quote color-blue",
+        "render": "/App_Plugins/LeBlender/editors/leblendereditor/views/Base.cshtml",
+        "config": {
+            "editors": [
+                {
+                    "name": "Icon Image",
+                    "alias": "iconImage",
+                    "propretyType": {},
+                    "dataType": "135d60e0-64d9-49ed-ab08-893c9ba44ae5"
+                },
+                {
+                    "name": "Heading",
+                    "alias": "heading",
+                    "propretyType": {},
+                    "dataType": "0cc0eba1-9960-42c9-bf9b-60e150b429ae"
+                },
+                {
+                    "name": "Text",
+                    "alias": "text",
+                    "propretyType": {},
+                    "dataType": "c6bac0dd-4ab9-45b1-8e30-e4b619ee5da3"
+                }
+            ],
+            "frontView": ""
+        }
+    },
+    {
+        "name": "Companies Banner",
+        "alias": "companiesBanner",
+        "view": "/App_Plugins/LeBlender/editors/leblendereditor/LeBlendereditor.html",
+        "icon": "icon-company color-green",
+        "render": "/App_Plugins/LeBlender/editors/leblendereditor/views/Base.cshtml",
+        "config": {
+            "editors": [
+                {
+                    "name": "Heading",
+                    "alias": "heading",
+                    "propretyType": {},
+                    "dataType": "0cc0eba1-9960-42c9-bf9b-60e150b429ae"
+                },
+                {
+                    "name": "Text",
+                    "alias": "text",
+                    "propretyType": {},
+                    "dataType": "c6bac0dd-4ab9-45b1-8e30-e4b619ee5da3"
+                },
+                {
+                    "name": "Company Logos",
+                    "alias": "companyLogos",
+                    "propretyType": {},
+                    "dataType": "9dbbcbbb-2327-434a-b355-af1b84e5010a"
+                }
+            ],
+            "frontView": ""
+        }
     }
 ]
